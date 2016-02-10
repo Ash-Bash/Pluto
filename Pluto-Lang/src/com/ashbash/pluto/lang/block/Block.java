2319c47266485426361c9d5472c866fcd6d2f614
Ashley Chapman <xpsx.fan@googlemail.com>
2016-02-10 17:51:13 +0000

    Implemented The Parser Class and Improved The Block Class

--- Pluto-Lang/src/com/ashbash/pluto/lang/block/Block.java
+++ Pluto-Lang/src/com/ashbash/pluto/lang/block/Block.java
@@ -12,15 +12,19 @@ import java.util.ArrayList;
 public abstract class Block {
 
     private Block superBlock;
-    private ArrayList<Block> subBlock;
+    private ArrayList<Block> subBlocks;
 
     public Block(Block superBlock) {
         this.superBlock = superBlock;
-        this.subBlock = new ArrayList<>();
+        this.subBlocks = new ArrayList<>();
     }
 
     public Block getSuperBlock() {
         return superBlock;
+    }
+
+    public void addBlock(Block block) {
+        subBlocks.add(block);
     }
 
     public abstract void run();
--- Pluto-Lang/src/com/ashbash/pluto/lang/parser/Parser.java
+++ Pluto-Lang/src/com/ashbash/pluto/lang/parser/Parser.java
@@ -1,7 +1,20 @@
 package com.ashbash.pluto.lang.parser;
 
+import com.ashbash.pluto.lang.block.Block;
+import com.ashbash.pluto.lang.tokenizer.Tokenizer;
+
 /**
  * Created by ashleychapman on 10/02/2016.
  */
-public class Parser {
+public abstract class Parser<T extends Block> {
+
+    /**
+     * Tokes a Line and Checks to See if it is for this parser by using regex
+     */
+    public abstract boolean shouldParse(String line);
+
+    /**
+     *  Toke the SuperBlock and The Tokanizer for The Line and Return a Block of this Parser's Type.
+     */
+    public abstract T parse(Block superBlock, Tokenizer tokenizer);
 }
